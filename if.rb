a9a6941c99607caf69805ed157859d8909e98ee3
Sugi-Hi <sugi-hi2017@zd.ztv.ne.jp>
2024-05-23 11:25:37 +0900

    条件文ファイル修正

--- if.rb
+++ if.rb
@@ -12,10 +12,10 @@ end
 
 input = gets.to_i
 
-if input <= 10
-   puts "10以下の数字です"
-elsif input <= 0
+if input <= 0
    puts "0以下の数字です"
+elsif input <= 10
+   puts "10以下の正の数字です"
 else
    puts "10より大きい数字です"
 end
