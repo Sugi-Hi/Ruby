f3694917066749502f8fae54b48840d6f8797ca6
Sugi-Hi <sugi-hi2017@zd.ztv.ne.jp>
2024-05-25 23:54:03 +0900

    ループファイル好色の追加

--- loop.rb
+++ loop.rb
@@ -1,9 +1,16 @@
 colors = ["あか", "あお", "きいろ", "みどり", "しろ", "くろ", "むらさき"]
 
 colors.each_with_index do |color,index|
-  
   puts "(#{index+1})色: #{color}"
 end
+
+favorite_colors = []
+
+puts "Please input your favorite color number from 1 to 7!(What's your favorite color?)"
+input_num = gets.to_i
+favorite_colors << colors[input_num-1]
+
+favorite_color 
 
 while true do
   exit
