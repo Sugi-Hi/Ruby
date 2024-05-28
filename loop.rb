01b6254766b8f887025646a6e65d1caac5cf6465
Sugi-Hi <sugi-hi2017@zd.ztv.ne.jp>
2024-05-28 11:32:21 +0900

    ループファイル好色修正1

--- loop.rb
+++ loop.rb
@@ -14,9 +14,17 @@ while true do
     puts "Your favorite color is (#{colors[input_num-1]})!!"
     favorite_colors << colors[input_num-1]
     puts "Another more!? If nothing,please input other any numbers!"
+
+    if (input_num > 0) && (input_num <= 7)
+      puts "Your favorite colors are (#{favorite_colors})!!"
+    else
+      puts "Thank you for using !!See you!!"
+      exit
+    end
+
   else
-    puts "Thank you for using!!Good by!!"
+    puts "Thank you for using!!Good bye!!"
     exit
   end
-  puts "Your favorite colors are (#{favorite_colors})!!"
+
 end
