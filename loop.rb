a80e5b2398101f110ed8d6f202a3821f0f82acda
Sugi-Hi <sugi-hi2017@zd.ztv.ne.jp>
2024-05-31 18:23:43 +0900

    ループファイル選択色カスタマイズ

--- loop.rb
+++ loop.rb
@@ -5,19 +5,24 @@ default_colors.each_with_index do |color,index|
   puts "#{index+1}.#{color}色"
 end
 
+colors = ["みどり", "むらさき", "ちゃいろ", "だいだい", "はだ", "もも", "みず", "はいいろ", "こんいろ"]
+
 custom_colors = default_colors
+while true do
+end
+
+
 
 favorite_colors = []
-
 while true do
   puts "Please input your favorite color on above list number from 1 to last number,if anything!(What's your favorite color?)"
   input_num = gets.to_i
 
-  if (input_num > 0) && (input_num <= )
-    favorite_colors << colors[input_num-1]
+  if (input_num > 0) && (input_num <= custom_colors.length)
+    favorite_colors << custom_colors[input_num-1]
     
     if favorite_colors.length == 1
-      puts "Your favorite color is (#{colors[input_num-1]})!!"
+      puts "Your favorite color is (#{custom_colors[input_num-1]})!!"
       puts "Another more!? If nothing,please input other any numbers!"
     else
       puts "Your favorite colors are (#{favorite_colors})!!"
