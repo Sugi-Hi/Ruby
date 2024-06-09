17702cf1ae4941fc814a5d2dcc2d0d846dfc527a
Sugi-Hi <sugi-hi2017@zd.ztv.ne.jp>
2024-06-09 11:10:59 +0900

    秒ファイル時間追加

--- seconds.rb
+++ seconds.rb
@@ -2,7 +2,8 @@ puts "ようこそ！名前を入力してください"
 name = gets.chomp
 
 puts "秒数を知りたい時間を分単位で入力してください"
+hours = gets.to_i
 minutes = gets.to_i
-seconds = minutes * 60
+seconds = hours * 3600 + minutes * 60
 
-puts "#{name}さん、#{minutes}分は#{seconds}秒です！"
+puts "#{name}さん、#{minutes}時間#{minutes}分は#{seconds}秒です！"
