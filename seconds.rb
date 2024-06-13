1e8b8ba454b2ef3d0fdb7734a8ef00619a5314c5
Sugi-Hi <sugi-hi2017@zd.ztv.ne.jp>
2024-06-13 11:29:15 +0900

    秒ファイル時分の正しい入力までループ

--- seconds.rb
+++ seconds.rb
@@ -4,21 +4,29 @@ name = gets.chomp
 
 puts "秒数を知りたい時間を時(0~24~)単位で入力してください。"
 while true do
-hours = gets.to_i
-  if hours >= 0
-    puts "#{hours}時間:#{hours * 3600}秒"
-    break
-  else
-    puts "秒数を知りたい時間を整数(0~24~)で入力し直してください。"
-  end
+  hours = gets.to_i
+    if hours >= 0
+      puts "#{hours}時間:#{hours * 3600}秒"
+      break
+    else
+      puts "時間を整数(0~24~)で入力し直してください。"
+    end
 end
 
 
 puts "秒数を知りたい時間を分(0~60)単位で入力してください。"
+loop{
+
 minutes = gets.to_i
+  if minutes >= 0
+    puts "#{minutes}分:#{minutes * 60}秒 "
+    break
+  else
+    puts "何分か整数(0~60~)で入力し直してください。"
+    redo
+  end
+
+}
+
 seconds = hours * 3600 + minutes * 60
-
-puts "#{hours}時間:#{hours * 3600}秒 #{minutes}分:#{minutes * 60}秒 "
-
-
 puts "#{name}さん、秒数でお伝えします。 \n#{hours}時間#{minutes}分は#{seconds}秒です！"
