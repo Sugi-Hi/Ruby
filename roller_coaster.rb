b4a508e902adeae5f599f1de62154dfcd7bf874a
Sugi-Hi <sugi-hi2017@zd.ztv.ne.jp>
2024-06-19 17:22:42 +0900

    コースターファイル：乗車可否の集計数

--- roller_coaster.rb
+++ roller_coaster.rb
@@ -1,43 +1,28 @@
 ride_count = 0
 friends = []
-
 friend = {}
-puts "お友達の名前は？"
-friend[:name] = gets.chomp
-puts "お友達の身長は？"
-friend[:height] = gets.to_i
-friends << friend
-if friend[:height] >= 130
-  puts "#{friend[:name]}くんは乗車できます"
-  ride_count += 1
-else
-  puts "#{friend[:name]}くんは乗車できません！"
+
+puts "お友達は何人いますか？"
+friends_number = gets.to_i
+
+
+friends_number.times do |fn|
+  puts "--------------------------"
+  puts "お友達#{fn+1}人目"
+  puts "お友達の名前は？"
+  friend[:name] = gets.chomp
+  puts "お友達の身長は？"
+  friend[:height] = gets.to_i
+  friends << friend
+
+  if friend[:height] >= 130
+    puts "#{friend[:name]}くんは乗車できます"
+    ride_count += 1
+  else
+    puts "#{friend[:name]}くんは乗車できません！"
+  end
+
 end
 
-friend = {}
-puts "お友達の名前は？"
-friend[:name] = gets.chomp
-puts "お友達の身長は？"
-friend[:height] = gets.to_i
-friends << friend
-if friend[:height] >= 130
-  puts "#{friend[:name]}くんは乗車できます"
-  ride_count += 1
-else
-  puts "#{friend[:name]}くんは乗車できません！"
-end
-
-friend = {}
-puts "お友達の名前は？"
-friend[:name] = gets.chomp
-puts "お友達の身長は？"
-friend[:height] = gets.to_i
-friends << friend
-if friend[:height] >= 130
-  puts "#{friend[:name]}くんは乗車できます"
-  ride_count += 1
-else
-  puts "#{friend[:name]}くんは乗車できません！"
-end
 
 puts "乗車するのは#{ride_count}人です"
