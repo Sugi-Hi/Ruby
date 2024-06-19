6b2fbab3aae0af0c667c0677e72d02b669a2a2b1
Sugi-Hi <sugi-hi2017@zd.ztv.ne.jp>
2024-06-19 16:30:38 +0900

    受験生徒ファイル：見分けやすい出力へ修正

--- student.rb
+++ student.rb
@@ -3,6 +3,7 @@ def register_student(students)
   name = gets.chomp
   puts "何組か入力して下さい:"
   room = gets.chomp
+  puts "................................................."
   puts "国語の点数を入力して下さい:"
   japanese = gets.to_i
   puts "数学の点数を入力して下さい:"
@@ -22,31 +23,40 @@ def register_student(students)
 end
 
 def index_student(students) # 全受験生徒における一覧の表示
-  puts "-----------------------------------------------"
+  puts "----------------------------------------------------------------------------------------------"
   puts "生徒一覧表#受験者"
+  puts "................................................."
   students.each_with_index do |student, index|  # index:初期値0、数値化できない!
-  puts "受験番号： 00#{index+1}  氏名：#{student[:name]} 、#{student[:room]}組"
+  puts "受験番号：[00#{index+1}]  氏名：#{student[:name]} 、#{student[:room]}組"
   end
   puts "受験生徒数：#{students.length}名"
-  puts "-----------------------------------------------"
+  puts "----------------------------------------------------------------------------------------------"
   average(students) # 全生徒の受験した各5教科の平均点：students.each do |s|  // Jap+=s[:jap]/students.length, Math+=s[:math]/students.length, Social+=s[:social]/students.length, Science+=s[:science]/students.length, Eng+=s[:eng]/students.length // end
-
+  puts "................................................."
   puts "見たい生徒を受験番号(上位ケタの0を抜いて)で選択入力して下さい！"
+
+  loop{
   input = gets.to_i
 
   if input > 0 && input <= students.length
     show_student(students[input-1]) # 指定受験生徒の詳細：配列データ要素の抽出
+    break
   else
     puts "入力した受験番号は存在しません。再度を選択し直して下さい。"
+    redo
   end
+  }
 
 end
 
-def show_student(student) # 選択した受験生徒の詳細
+def show_student(student) # 選択した受験生徒の詳細]
+  puts "----------------------------------------------------------------------------------------------"
   puts "氏名：#{student[:name]} 、#{student[:room]}組"
+  puts "................................................."
   puts "国語：#{student[:jap]}点、数学：#{student[:math]}点、社会：#{student[:social]}点、理科：#{student[:science]}点、英語：#{student[:eng]}点"
   puts "5教科の合計点⇒#{student[:sum]}点"  # 各生徒の5教科合計点：{student[:jap]+student[:math]+student[:social]+student[:science]+student[:eng]}
   puts "偏差値：計算中"
+  puts "----------------------------------------------------------------------------------------------"
 end
 def average(students) # 平均点
   sum_jap=0
@@ -88,6 +98,7 @@ while true
     exit
   else 
     puts "無効な受付番号なので再度0~2で選択入力して下さい!"
+    puts "----------------------------------------------------------------------------------------------"
   end
 
 end
