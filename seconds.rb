94aa5607efe0c0626c6fcf56d7901f3d52785712
Sugi-Hi <sugi-hi2017@zd.ztv.ne.jp>
2024-06-19 16:50:19 +0900

    秒ファイル：適切入力まで見分けやすいループ出力

--- seconds.rb
+++ seconds.rb
@@ -5,13 +5,14 @@ while true do
 
   if hours >= 0
     puts "#{hours}時間:#{hours * 3600}秒"
+    puts "→→→→→→→→→→→→→→→→→→→→→→"
     break
   else
     puts "時間を正の整数(0~24~)で入力し直してください。"
+    puts "←←←←←←←←←←←←←←←←←←←←←←"
   end
 
 end
-
 
 puts "かけられる時間を分(0~60~)単位で入力してください。"
 
@@ -20,31 +21,37 @@ while true do
 
   if minutes >= 0 && minutes < 60
     puts "#{minutes}分:#{minutes * 60}秒 "
+    puts "→→→→→→→→→→→→→→→→→→→→→→"
     break
   elsif minutes >= 60
     hours += minutes / 60
     puts "#{hours}時間:#{hours * 3600}秒"
     minutes = minutes % 60
     puts "#{minutes}分:#{minutes * 60}秒"
+    puts "→→→→→→→→→→→→→→→→→→→→→→"
     break
   else
     puts "何分か正の整数(0~60~)で入力し直してください。"
+    puts "←←←←←←←←←←←←←←←←←←←←←←"
   end
 
 end
 
 if hours == 0 && minutes == 0
+  puts "--------------------------------------------"
   puts "時間が無く、進められません…"
   exit
 end
 
 seconds = hours * 3600 + minutes * 60
 
+puts "--------------------------------------------"
+puts "名前(ニックネームなど)を入力してください。"
 loop {
-  puts "名前(ニックネームなど)を入力してください。"
   name = gets.chomp
   if name == ""
-    puts "空白にせず何か入力してください。"
+    puts "空白にせず何か名前(イニシャルなど)を入力してください。"
+    puts "←←←←←←←←←←←←←←←←←←←←←←"
     redo
   else
     puts "#{name}さん、知りたい所要時間を秒数でお伝えします。 \nかけられる#{hours}時間#{minutes}分は、#{seconds}秒ほどです！"
@@ -52,8 +59,9 @@ loop {
 
 
 walk_speed = ["遅い", "やや遅い", "普通", "やや速い", "速い"]
-
+puts "--------------------------------------------"
 puts "歩くスピードについてお聞きしますが、次の番号1～5の中から選択してください。"
+puts "→→→→→→→→→→→→→→→→→→→→→→"
 while true do
 
   walk_speed.each_with_index do |speed,num|
@@ -66,10 +74,12 @@ while true do
   distance = (seconds*(speed*(1000.0/3600.0))).to_f.round(0) # 歩行スピード単位(自然数メートル表示)：km/h⇒m/s
   kilo_distance = (distance/1000.0).to_f.round(2)  # 歩行スピード単位(小数第2位実数キロメートル表示)
   if input > 0 && input < 6
+    puts "--------------------------------------------"
     puts "#{name}さん、これから#{distance}m(#{kilo_distance}km)ほど進めます!" 
     exit
   else 
     puts "番号1～5の中から選択し直してください。" 
+    puts "←←←←←←←←←←←←←←←←←←←←←←"
   end
   
 end
