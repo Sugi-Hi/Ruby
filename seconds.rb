e0881d5f39545c23bbeed740733fe591baf50b95
Sugi-Hi <sugi-hi2017@zd.ztv.ne.jp>
2024-06-19 13:46:31 +0900

    秒ファイル：詳しい案内文へ修正

--- seconds.rb
+++ seconds.rb
@@ -2,29 +2,35 @@
 puts "かけられる時間を時(0~24~)単位で入力してください。"
 while true do
   hours = gets.to_i
-    if hours >= 0
-      puts "#{hours}時間:#{hours * 3600}秒"
-      break
-    else
-      puts "時間を整数(0~24~)で入力し直してください。"
-    end
+
+  if hours >= 0
+    puts "#{hours}時間:#{hours * 3600}秒"
+    break
+  else
+    puts "時間を正の整数(0~24~)で入力し直してください。"
+  end
+
 end
 
 
 puts "かけられる時間を分(0~60~)単位で入力してください。"
 
 while true do
-minutes = gets.to_i
+  minutes = gets.to_i
+
   if minutes >= 0 && minutes < 60
     puts "#{minutes}分:#{minutes * 60}秒 "
     break
   elsif minutes >= 60
     hours += minutes / 60
-    minutes += minutes % 60
+    puts "#{hours}時間:#{hours * 3600}秒"
+    minutes = minutes % 60
+    puts "#{minutes}分:#{minutes * 60}秒"
     break
   else
-    puts "何分か整数(0~60~)で入力し直してください。"
+    puts "何分か正の整数(0~60~)で入力し直してください。"
   end
+
 end
 
 if hours == 0 && minutes == 0
@@ -34,20 +40,16 @@ end
 
 seconds = hours * 3600 + minutes * 60
 
-loop{
-
+loop {
   puts "名前(ニックネームなど)を入力してください。"
-  name = gets.chomp 
-  
+  name = gets.chomp
   if name == ""
     puts "空白にせず何か入力してください。"
     redo
   else
-    puts "#{name}さん、知りたい時間を秒数でお伝えします。 \n可能な#{hours}時間#{minutes}分は、#{seconds}秒ほどです！"
-    break
+    puts "#{name}さん、知りたい所要時間を秒数でお伝えします。 \nかけられる#{hours}時間#{minutes}分は、#{seconds}秒ほどです！"
   end
 
-}
 
 walk_speed = ["遅い", "やや遅い", "普通", "やや速い", "速い"]
 
@@ -59,13 +61,14 @@ while true do
   end
 
   input = gets.to_i
-  distance = ((input+1)*seconds)*1000/3600  # 歩行スピード単位(メートル表示)：km/h⇒m/s
-  kilo_distance = ((input+1)*seconds)/3600  # 歩行スピード単位(キロメートル表示)
+  distance = ((input+1)*seconds)*1000/3600  # 歩行スピード単位(自然数メートル表示)：km/h⇒m/s
+  kilo_distance = ((input+1)*seconds)/3600  # 歩行スピード単位(正の整数キロメートル表示)
   if input > 0 && input < 6
-    puts "これから#{distance}m(#{kilo_distance}km)ほど進めます!" 
+    puts "#{name}さん、これから#{distance}m(#{kilo_distance}km)ほど進めます!" 
     exit
   else 
     puts "番号1～5の中から選択し直してください。" 
   end
   
 end
+}
