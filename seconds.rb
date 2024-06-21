efd3c26cbc79e2f9a7ad0a45acd9b0dfb7b25188
Sugi-Hi <sugi-hi2017@zd.ztv.ne.jp>
2024-06-21 20:37:02 +0900

    秒ファイル：小数点以下の表示訂正

--- seconds.rb
+++ seconds.rb
@@ -1,6 +1,6 @@
 #適した入力までのループ処理
 puts "かけられる時間を時(0~24~)単位で入力してください。"
-while true do
+while true
   hours = gets.to_i
 
   if hours >= 0
@@ -16,7 +16,7 @@ end
 
 puts "かけられる時間を分(0~60~)単位で入力してください。"
 
-while true do
+while true
   minutes = gets.to_i
 
   if minutes >= 0 && minutes < 60
@@ -47,7 +47,7 @@ seconds = hours * 3600 + minutes * 60
 
 puts "--------------------------------------------"
 puts "名前(ニックネームなど)を入力してください。"
-loop {
+loop{
   name = gets.chomp
   if name == ""
     puts "空白にせず何か名前(イニシャルなど)を入力してください。"
@@ -71,11 +71,11 @@ while true do
   input = gets.to_i
 
   speed = (speed_rank[input-1][:speed]).to_f
-  distance = (seconds*(speed*(1000.0/3600.0))).to_f.round(0) # 歩行スピード単位(自然数メートル表示)：km/h⇒m/s
-  kilo_distance = (distance/1000.0).to_f.round(2)  # 歩行スピード単位(小数第2位実数キロメートル表示)
+  distance = (seconds*(speed*(1000.0/3600.0))).to_f # 歩行スピード単位(自然数メートル表示)：km/h⇒m/s
+  kilo_distance = (distance/1000.0).to_f  # 歩行スピード単位(小数第2位実数キロメートル表示)
   if input > 0 && input < 6
     puts "--------------------------------------------"
-    puts "#{name}さん、これから#{distance}m(#{kilo_distance}km)ほど進めます!" 
+    puts "#{name}さん、これから#{distance.to_f.round(0)}m(#{kilo_distance.to_f.round(2)}km)ほど進めます!" 
     exit
   else 
     puts "番号1～5の中から選択し直してください。" 
