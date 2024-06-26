56af1582d755677b44b0dbac62ed2384ef11da26
Sugi-Hi <sugi-hi2017@zd.ztv.ne.jp>
2024-06-26 18:16:29 +0900

    メソッド集ファイル：日付用今日メソッド

--- method.rb
+++ method.rb
@@ -1,3 +1,5 @@
+require 'date'
+
 def mixer(fruit)
   puts "#{fruit}を細かく砕く"
   return "#{fruit}ジュース"
@@ -11,8 +13,9 @@ puts mixer(input)
 num = Math.sqrt(9)
 num1 = Math.sqrt(3)
 time = Time.now
-
+date = Date.today
 
 puts "ルート9=#{num.to_i}"
 puts "ルート3=#{num1.to_f.round(3)}"
 puts "現在:#{time}"
+puts "今日:#{date}"
