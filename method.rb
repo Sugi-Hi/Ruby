1e2f687ebe657a63755715bfe6df5b7906cdcd00
Sugi-Hi <sugi-hi2017@zd.ztv.ne.jp>
2024-06-27 10:58:42 +0900

    メソッド集ファイル：曜日の表し方

--- method.rb
+++ method.rb
@@ -15,10 +15,13 @@ num1 = Math.sqrt(3)
 time = Time.now
 date = Date.today
 
+days = ["日", "月", "火", "水", "木", "金", "土"]
+
 puts "ルート9=#{num.to_i}"
 puts "ルート3=#{num1.to_f.round(3)}\n"
 
 puts "現在:#{time}"
 puts "................"
 puts "時刻　#{time.strftime("%H:%M:%S")}"
-puts "今日　#{date}\n"
+puts "今日　#{date}"
+puts "今曜日#{date.strftime("%Y-%m-%d(%u)")}\n"
