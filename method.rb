6bb3344428dc7dcb959cf6f50a6b86166f4c4e06
Sugi-Hi <sugi-hi2017@zd.ztv.ne.jp>
2024-06-28 18:36:11 +0900

    メソッド集ファイル：曜日表示の追加

--- method.rb
+++ method.rb
@@ -16,6 +16,7 @@ time = Time.now
 date = Date.today
 
 days = ["日", "月", "火", "水", "木", "金", "土"]
+day = date.strftime("%u").to_i
 
 puts "ルート9=#{num.to_i}"
 puts "ルート3=#{num1.to_f.round(3)}\n"
@@ -24,4 +25,4 @@ puts "現在:#{time}"
 puts "................"
 puts "時刻　#{time.strftime("%H:%M:%S")}"
 puts "今日　#{date}"
-puts "今曜日#{date.strftime("%Y-%m-%d(%u)")}\n"
+puts "曜日　#{date.strftime("%Y-%-m-%-d(#{days[day]})")}\n"
