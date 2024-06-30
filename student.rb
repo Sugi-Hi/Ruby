3a3d1f2ea0720665645e5224cd28daa161eb287e
Sugi-Hi <sugi-hi2017@zd.ztv.ne.jp>
2024-06-30 11:22:55 +0900

    受験生徒ファイル：合計点の高い順番

--- student.rb
+++ student.rb
@@ -104,8 +104,7 @@ end
 
 # \\\\\\\\\\\\\\\\\\\ 合計点・偏差値 \\\\\\\\\\\\\\\\\\\
 def rank_student(students)
-  students = students[:sum].max(students.length)
-
+  (students[:sum].to_i).sort{ |start, learn| learn <=> start }
   students.each_with_index do |student, rank|
     puts "#{rank+1}位：#{student[:sum]}点、偏差値#{student[:hensa]}"
   end
